8ebb11313b06c6f617588365d7423ef8cf510a3a
Shinji Kusakabe <skusakabe30@stuy.edu>
2022-09-21 14:06:14 -0400

    added not string

--- 03_py/warmup1/warmup1.py
+++ 03_py/warmup1/warmup1.py
@@ -89,6 +89,16 @@ def pos_neg(a, b, negative):
 # pos_neg(-4, -5, True) → True
 
 
+# Given 2 int values, return True if one is negative and one is positive. Except if the parameter "negative" is True, then return True only if both are negative.
+def not_string(str):
+  if (str[:3] == "not"):
+    return str
+  return "not " + str
+
+# pos_neg(1, -1, False) → True
+# pos_neg(-1, 1, False) → True
+# pos_neg(-4, -5, True) → True
+
 
 # Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged.
 def front_back(str):

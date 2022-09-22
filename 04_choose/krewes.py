f71587c66870194810a88ede88f5c7c6c28f031f
Shinji Kusakabe <skusakabe30@stuy.edu>
2022-09-22 14:04:37 -0400

    Found a way to take a random value from a dictionary

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -1,3 +1,11 @@
 import random as rng
 
-#krewes = {}
+krewes = {2:[1,2,3], 7:[4,5,6], 8:[7,8,9]}
+
+def randdevo(dnary):
+    alldevos = []
+    for i in (dnary.keys()):
+        alldevos += dnary[i]
+    return rng.choice(alldevos)
+
+print(randdevo(krewes))

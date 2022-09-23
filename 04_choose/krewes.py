ff7f5a98a8c9f690c5a830b6451c7a558b4f745a
Shinji Kusakabe <skusakabe30@stuy.edu>
2022-09-23 13:43:04 -0400

    tested our method with the test cases

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -1,11 +1,21 @@
 import random as rng
 
-krewes = {2:[1,2,3], 7:[4,5,6], 8:[7,8,9]}
+krewes1 = {2:["A", "B", "C"], 3:["D", "E", "F"]}
+krewes2 = {
+    2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY", "Ruiwen"],
+    7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
+    8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "Wanying", "Kevin"]
+}
+krewes3 = {}
 
 def randdevo(dnary):
-    alldevos = []
-    for i in (dnary.keys()):
-        alldevos += dnary[i]
-    return rng.choice(alldevos)
+    try:
+        alldevos = []
+        for i in (dnary.keys()):
+            alldevos += dnary[i]
+        return rng.choice(alldevos)
+    except:
+        print("Error, dict is length 0")
 
-print(randdevo(krewes))
+for i in range(20):
+    print(randdevo(krewes2))

5e0a851480772876e39e6dfd7e7ec10d3a0e6200
ShinjiK1 <92060952+ShinjiK1@users.noreply.github.com>
2022-10-02 17:45:24 -0400

    Finished dictionary creation method, now have to work on taking a weighted random

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,17 +1,33 @@
+'''
+Shinji Verit
+SoftDev
+K06 duo mission
+2022-09-30
+Time spent: 0.8 hours
+
+DISCO:
+
+QCC:
+'''
+
 file = open("occupations.csv", "r")
 unparsed = file.read()
 
 def csv_to_dict(data):
     result = {}
     lines = data.split("\n")
+    lines = lines[1:len(lines)-1]
     for oneline in lines:
         if '"' in oneline:
             oneline = oneline[1::]
             values = oneline.split('"')
 #           print(oneline) -> shows that oneline stays unchanged when using oneline.split()
             values[1] = values[1][1::]
-            print(values)
+            result[values[0]] = float(values[1])
         else:
             values = oneline.split(",")
+            result[values[0]] = float(values[1])
+    return result
 
-csv_to_dict(unparsed)
+a = csv_to_dict(unparsed)
+print(a)

2740fd80a27548508183ad068830b07c983108e0
ShinjiK1 <92060952+ShinjiK1@users.noreply.github.com>
2022-10-08 14:22:20 -0400

    Finished basics I think

--- 08_serve/app.py
+++ 08_serve/app.py
@@ -16,7 +16,8 @@ def csv_to_dict(file):
     for oneline in lines:
         if '"' in oneline: #checks for when there is an occupation containing commas
             oneline = oneline[1::] #removes first quotation mark
-            values = oneline.split('"') #splits string on second quotation mark, creating a list with the two halves and removes the quotation mark            values[1] = values[1][1::] #removes the unwanted comma currently part of the string percentage: ",6.1" --> "6.1"
+            values = oneline.split('"') #splits string on second quotation mark, creating a list with the two halves and removes the quotation mark
+            values[1] = values[1][1::] #removes the unwanted comma currently part of the string percentage: ",6.1" --> "6.1"values[1] = values[1][1::] #removes the unwanted comma currently part of the string percentage: ",6.1" --> "6.1"
             result[values[0]] = float(values[1]) #adds occupation,percentage pair to dictionary while converting string percentage to float
         else:
             values = oneline.split(",") #splits string on comma, creating a list with the two halves and removes the comma

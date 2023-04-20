9901be63ce263661a037556835eab00ba87176ce
Shinji Kusakabe <skusakabe30@stuy.edu>
2023-04-20 13:50:26 -0400

    a"

---
+++ 29_jsdom/dommy_consolery.js
@@ -0,0 +1,138 @@
+/*
+   your PPTASK:
+   
+   Test drive each bit of code in this file,
+    and insert comments galore, indicating anything
+     you discover,
+    	have questions about,
+    		or otherwise deem notable.
+    		
+    		Write with your future self or teammates in mind.
+    		
+    		If you find yourself falling out of flow mode, consult 
+    		other teams
+    		MDN
+
+   A few comments have been pre-filled for you...
+   
+   (delete this block comment once you are done)
+*/
+
+// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
+// SoftDev pd0
+// K28 -- Getting more comfortable with the dev console and the DOM
+// 2023-04-05w
+// --------------------------------------------------
+
+
+//send diagnostic output to console
+//(Ctrl-Shift-K in Firefox to reveal console)
+console.log("AYO");
+
+var i = "hello";
+var j = 20;
+
+
+//assign an anonymous fxn to a var
+var f = function(x) {
+  var j=30;
+  return j+x;
+};
+
+
+//instantiate an object
+var o = { 'name' : 'Thluffy',
+          age : 1024,
+          items : [10, 20, 30, 40],
+          morestuff : {a : 1, b : 'ayo'},
+          func : function(x) {
+            return x+30;
+          }
+        };
+
+
+var addItem = function(text) {
+  var list = document.getElementById("thelist");
+  var newitem = document.createElement("li");
+  newitem.innerHTML = text;
+  list.appendChild(newitem);
+};
+
+
+var removeItem = function(n) {
+  var listitems = document.getElementsByTagName('li');
+  listitems[n].remove();
+};
+
+
+var red = function() {
+  var items = document.getElementsByTagName("li");
+  for(var i = 0; i < items.length; i++) {
+    items[i].classList.add('red');
+  }
+};
+
+
+var stripe = function() {
+  var items = document.getElementsByTagName("li");
+  for(var i = 0; i < items.length; i++) {
+    if (i%2==0){
+      items[i].classList.add('red');
+    } else {
+      items[i].classList.add('blue');
+    }
+  }
+};
+
+//insert your implementations here for...
+// FIB
+var fib = function(n) {
+    if (n < 2) {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+// FAC
+var fact = function(n) {
+    if (n < 2) {
+        return 1;
+    }
+    return n * fact(n - 1);
+}
+// GCD
+var gcd = function (a, b) {
+  if (a > b) {
+    if (a % b == 0) {
+        return b;
+    }
+    return gcd(a, a % b);   
+  }
+  if (b % a == 0) {
+    return a;
+  }
+  return gcd(b, b % a);
+}
+
+// In addition to the style shown above,
+//  you are encouraged to test drive the "arrow function syntax" as shown below.
+//  Note anything notable.
+const myFxn = (param1, param2) => {
+  // body
+  return retVal;
+};
+
+var fibbut = document.getElementById("fib");
+var factbut = document.getElementById("fact");
+var gcdbut = document.getElementById("gcd");
+
+fibbut.addEventListener('click', function () {
+    addItem(fib(6))
+});
+factbut.addEventListener('click', function () {
+    addItem(fact(6))
+});
+gcdbut.addEventListener('click', function () {
+    addItem(gcd(15, 6))
+});
+
+

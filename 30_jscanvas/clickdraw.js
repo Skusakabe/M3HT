27d78303a4326b0f5ce20eadbcf967bebac08327
ShinjiK1 <92060952+ShinjiK1@users.noreply.github.com>
2023-04-24 20:52:45 -0400

    worked on 30

--- 30_jscanvas/clickdraw.js
+++ 30_jscanvas/clickdraw.js
@@ -1,6 +1,9 @@
 //retrieve node in DOM via ID
 var c = document.getElementById("slate");
 var wipe = document.getElementById("buttonClear");
+var modeToggle = document.getElementById("buttonToggle");
+const xOffset = 8;
+const yOffset = 132;
 
 //instantiate a CanvasRenderingContext2D object
 var ctx = c.getContext("2d");
@@ -19,21 +22,36 @@ var toggleMode = (e) => {
     }
 }
 
-var drawRect = function(e) {
-    var mouseX = e.clientX - 8; //gets X-coor of mouse when event is fired
-    var mouseY = e.clientY - 132;//gets Y-coor of mouse when event is fired
+var drawRect = (e) => {
+    var mouseX = e.offsetX; //gets X-coor of mouse when event is fired
+    var mouseY = e.offsetY;//gets Y-coor of mouse when event is fired
     console.log("mouseclick registered at ", mouseX, mouseY);
+    ctx.beginPath();
     ctx.fillRect(mouseX, mouseY, 10, 20);
+    ctx.closePath();
 }
 
-c.addEventListener("click", function() {
+var drawCirc = (e) => {
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;
+    console.log("mouseclick registered at ", mouseX, mouseY);
+    ctx.beginPath();
+    ctx.arc(mouseX, mouseY, 8, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+}
+
+var draw = (e) => {
     if (mode === "rect") {
-        drawRect();
+        drawRect(e);
     }
     else {
-        drawCirc();
+        drawCirc(e);
     }
-}); //passes the mouse event as parameter for the function
+}
+
+c.addEventListener("click", draw); //passes the mouse event as parameter for the function
 wipe.addEventListener("click", function() {
     ctx.clearRect(0,0,600,600);
 });
+modeToggle.addEventListener("click", toggleMode);

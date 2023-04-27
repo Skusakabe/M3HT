f12b3ca84d63a250971474f1633aecff56e42e2e
ShinjiK1 <92060952+ShinjiK1@users.noreply.github.com>
2023-04-26 21:18:51 -0400

    finsihed

--- 32_jsanim/animate.js
+++ 32_jsanim/animate.js
@@ -15,15 +15,18 @@ ctx.fillStyle = "#ADD8E6";
 var requestID; //init global var for use with animation frames
 
 var clear = (e) => {
+    // e.preventDefault(); Seems to stop the circle animation as e.preventDefault is not a function.
+    // it will also break the dvdLogo button if the dvdLogo function has e as a parameter, but it will not
+    // break that button if only the dvdLogoSetup has e as a parameter.
     ctx.clearRect(0, 0,c.width,c.height);
 };
 
 var radius = 0;
 var growing = true;
 
-var drawDot = () => {
+var drawDot = (e) => {
     //clear the slate or playground
-    clear();
+    clear(e);
     //check if the circle is meant to grow AND diameter is larger than canvas
     if (growing === true && radius >= c.width / 2) {
         growing = false;
@@ -55,22 +58,24 @@ var drawDot = () => {
     requestID = window.requestAnimationFrame(drawDot);
 }
 
-var dvdLogoSetup = function () {
+var dvdLogoSetup = function (e) {
     window.cancelAnimationFrame(requestID);
-    var rectWidth = 20;
-    var rectHeight = 10;
+    var rectWidth = 60;
+    var rectHeight = 40;
 
-    var rectX = Math.floor(Math.random() * (ctx.width - rectWidth));
-    var rectY = Math.floor(Math.random() * (ctx.height - rectHeight));
+    var rectX = Math.floor(Math.random() * (c.width - rectWidth));
+    var rectY = Math.floor(Math.random() * (c.height - rectHeight));
 
-    var xVel = Math.floor(Math.random() * 8);
-    var yVel = Math.floor(Math.random() * 8);
+    // If you wanted the speed to always be the same, but the angle to be random, you could make a speed variable that stays constant and find a random angle.
+    // Then set xVel and yVel equal to that speed value multiplied by the cos(angle) or sin(angle) respectively.
+    var xVel = Math.random() * 2 + 1;
+    var yVel = Math.random() * 2 + 1;
 
     var logo = new Image();
     logo.src = "logo_dvd.jpg";
     var dvdLogo = function () {
         console.log(rectX);
-        ctx.clearRect(0,0,c.width,c.height);
+        clear(e);
         ctx.fillRect(rectX,rectY,rectWidth,rectHeight);
         ctx.drawImage(logo, rectX, rectY, rectWidth, rectHeight);
         if (rectX < 0 || rectX > c.width - rectWidth) {
@@ -78,7 +83,7 @@ var dvdLogoSetup = function () {
         }
         if (rectY < 0 || rectY > c.height - rectHeight) {
             yVel = yVel * -1;
-        }
+        }   
         rectX = rectX + xVel;
         rectY = rectY + yVel;
         requestID = window.requestAnimationFrame(dvdLogo);
